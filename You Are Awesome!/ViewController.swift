1e312781e1b5de02761d0435b9c0f0fc498a5ab1
Oscar <oscar.ramoschacon01@utrgv.edu>
2020-10-19 08:08:41 -0500

    Section 2.3 - done

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MainLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+         MainLabel.isHidden = true
+        
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func Show(_ sender: UIButton) {
+        
+        MainLabel.isHidden = false
+        
+    }
+    
+    
 
 }
 
