18fb593c83759f48fd371e06a4cc35ff3021ab6f
Oscar <oscar.ramoschacon01@utrgv.edu>
2020-10-18 21:01:24 -0500

    Changed to spanish

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Oscar Ramos Chacon on 10/18/20.
+//  Copyright © 2020 Oscar Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
